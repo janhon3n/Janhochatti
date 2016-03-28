9160cea8a563e30f8e4be121b94e2aca136f6211
Mikael Janhonen <miquel@Janho-WebServer>
2016-03-28 05:33:40 +0300

    Made changes to login to make it more reliable. A bunch of GUI updates and changes.

--- chatti.js
+++ chatti.js
@@ -29,87 +29,135 @@ var io = require('socket.io').listen(app.listen(port));
 io.on('connection', function(socket){
 	socket.channelsOn = [];
 	clients.push(socket);
-
-	socket.on('login', function(data, callback){
+	
+	socket.on('validateLogin', function(data, callback){
 		var username = validator.escape(data.username + '');
 		var color = validator.escape(data.color + '');
-		console.log(username + ' kirjautui chattiin');
-		socket.username = username;
-		socket.color = color;
-		callback();
-		joinChannel(socket, defaultChannel);
-	});
-
-	socket.on('create', function(data){
-		var channel = validator.escape(data.channel + '');
-		channels.push(channel);
-		socket.channelsOn.push(channel);
-		console.log(socket.username + ' created a new channel ' + channel);
-		socket.join(channel);
-
+		
+		var usernameInUse = false;
 		for(var i = 0; i < clients.length; i++){
-			clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
-		}
-
-		var msg = [];
-		var clrs = [];
-		msg[0] = socket.username;
-		clrs[0] = socket.color;
-		msg[1] = ' created a new channel ';
-		msg[2] = channel;
-		clrs[2] = 'gray';
-		io.to(channel).emit('info', {channel : channel, messages : msg, colors : clrs});
-	});
-
-	socket.on('join', function(data){
-		var channel = validator.escape(data.channel + '');
-		joinChannel(socket, channel);
-	});
-
-	socket.on('leave', function(data){
-		var channel = validator.escape(data.channel + '');
-		console.log(socket.username + ' left channel ' + channel);
-
-		//remove channel from clients list of active channels
-		var ind = socket.channelsOn.indexOf(channel);
-		if(ind != -1){
-			socket.channelsOn.splice(ind,1);
-		}
-		socket.leave(channel);
-
-		if(channelIsEmpty(channel)){
-			removeChannel(channel);
-			for(var i = 0; i < clients.length; i++){
-				clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
+			if(clients[i].username == username){
+				usernameInUse = true;
 			}
+		}
+		//if username already in use return error
+		if(usernameInUse){
+			callback('Username already in use');
 		} else {
-			socket.emit('updateChannels', {channelson : socket.channelsOn, channelsnoton : getChannelsNotOn(socket.channelsOn)});
-		}
-	});
+			
+			//else save user data and set up to wait for login
+			socket.username = username;
+			socket.color = color;
+		
+			socket.on('login', function(){
+				console.log(username + ' kirjautui chattiin');
+				
+				//after login set up functionality and join the default channel
+				socket.on('create', function(data, callback){
+					var channel = validator.escape(data.channel + '');
+					if(channels.indexOf(channel) == -1){
+						channels.push(channel);
+						socket.channelsOn.push(channel);
+						console.log(socket.username + ' created a new channel ' + channel);
+						socket.join(channel);
 
-	socket.on('disconnect', function(){
-		var ind = clients.indexOf(socket);
-		if(ind != -1){
-			clients.splice(ind, 1);
-		}
-		//check if channels client left are left empty => delete them
-		for(var i = 0; i < socket.channelsOn.length; i++){
-			if(channelIsEmpty(socket.channelsOn[i])){
-				removeChannel(socket.channelsOn[i]);
-			}
-		}
-		for(var i = 0; i < clients.length; i++){
-			clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
-		}
+						for(var i = 0; i < clients.length; i++){
+							clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
+						}
+						callback();
+					} else {
+						socket.emit('alert', {messages : ['Channel with same name already exists'], colors : []});
+						callback('Channel with same name already exists');
+					}
+				});
 
-		console.log(socket.username + ' disconnected');
-	});
+				socket.on('join', function(data, callback){
+					var channel = validator.escape(data.channel + '');
+					if(channels.indexOf(channel) >= 0){
+						if(socket.channelsOn.indexOf(channel) == -1){
+							joinChannel(socket, channel);
+							callback();
+						} else {
+							socket.emit('alert', {messages : ['You already are in that channel'], colors : []});
+							callback('You already are in that channel');
+						}
+					} else {
+						socket.emit('alert', {messages : ['Channel no longer exists'], colors : []});
+						callback('You already are in that channel');
+					}
+				});
 
-	socket.on('post', function(data){
-		var channel = validator.escape(data.channel + '');
-		var msg = validator.escape(data.message + '');
-		console.log(channel + '- ' + socket.username + ': ' + msg);
-		io.to(channel).emit('message', { channel : channel, username : socket.username, color : socket.color, message : msg });
+				socket.on('leave', function(data){
+					var channel = validator.escape(data.channel + '');
+					console.log(socket.username + ' left channel ' + channel);
+
+					//remove channel from clients list of active channels
+					var ind = socket.channelsOn.indexOf(channel);
+					if(ind != -1){
+						socket.channelsOn.splice(ind,1);
+					}
+					socket.leave(channel);
+
+					if(channelIsEmpty(channel)){
+						removeChannel(channel);
+						for(var i = 0; i < clients.length; i++){
+							clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
+						}
+					} else {
+						socket.emit('updateChannels', {channelson : socket.channelsOn, channelsnoton : getChannelsNotOn(socket.channelsOn)});
+					}
+
+					var msg = [];
+					var clrs = [];
+					msg[0] = socket.username;
+					clrs[0] = socket.color;
+					msg[1] = ' left the channel';
+					io.to(channel).emit('info', {channel : channel, messages : msg, colors : clrs});
+				});
+
+				socket.on('disconnect', function(){
+					var ind = clients.indexOf(socket);
+					if(ind != -1){
+						clients.splice(ind, 1);
+					}
+					//check if channels client left are left empty => delete them
+					for(var i = 0; i < socket.channelsOn.length; i++){
+						if(channelIsEmpty(socket.channelsOn[i])){
+							removeChannel(socket.channelsOn[i]);
+						}
+					}
+					for(var i = 0; i < clients.length; i++){
+						clients[i].emit('updateChannels', {channelson : clients[i].channelsOn, channelsnoton : getChannelsNotOn(clients[i].channelsOn)});
+					}
+
+					console.log(socket.username + ' disconnected');
+
+					var msg = [];
+					var clrs = [];
+					msg[0] = socket.username;
+					clrs[0] = socket.color;
+					msg[1] = ' disconnected';
+					for(var i = 0; i < socket.channelsOn.length; i++){
+						io.to(socket.channelsOn[i]).emit('info', {channel : socket.channelsOn[i], messages : msg, colors : clrs});
+					}
+				});
+
+				socket.on('post', function(data){
+					var channel = validator.escape(data.channel + '');
+					var msg = validator.escape(data.message + '');
+					console.log(channel + '- ' + socket.username + ': ' + msg);
+					io.to(channel).emit('message', { channel : channel, username : socket.username, color : socket.color, message : msg });
+				});
+				
+				socket.on('loginSucceful', function(){
+					socket.emit('updateChannels', {channelson : socket.channelsOn, channelsnoton : getChannelsNotOn(socket.channelsOn)});
+				});
+				
+				joinChannel(socket, defaultChannel);
+			});
+			
+			callback();
+		}
 	});
 });
 
--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,17 @@
 //messages are stored locally in client, list of all channels and channels client is on are stored in the server
 var defaultChannel = 'lobby';
+var defaultColors = ['#ff0000', '#FF00AF', '#8700FF', '#0012FF', '#00C1E2', '#02CE00', '#A6B300', '#FF9B00', '#9A0000', '#7B4000'];
 var messages = [];
+var maxMessagesPerChannel = 50;
+var socket;
+var scrollActive = true;
+
 messages[defaultChannel] = [];
 
 var currentChannel = '';
 
 $('document').ready(function(){
+	$('input#color').val(defaultColors[Math.floor(Math.random() * defaultColors.length)]);
 	$('#login button#loginbutton').click(function(){
 		login($('#usernamefield').val(), $('input#color').val());
 	});
@@ -17,89 +23,107 @@ $('document').ready(function(){
 });
 
 function login(usern, col){
-	$('#content').load('/chat.html', function(){
-		var socket = io();
-		socket.emit('login', {username : usern, color : col}, function(){
-			moveToChannel(defaultChannel);
+	socket = io();
+	socket.emit('validateLogin', {username : usern, color : col}, function(error){
+		if(error){
+			alert(error);
+			socket = null;
+		} else {
+			$('#content').load('/chat.html', function(){
 
-			//updates the channel list
-			socket.on('updateChannels', function(data){
-				var channelsNotOn = data.channelsnoton;
-				var channelsOn = data.channelson;
-				console.log('updating channels:');
-				console.log(channelsOn);
-				console.log(channelsNotOn);
+				moveToChannel(defaultChannel);
+
+				//updates the channel list
+				socket.on('updateChannels', function(data){
+					var channelsNotOn = data.channelsnoton;
+					var channelsOn = data.channelson;
+					console.log('updating channels:');
+					console.log(channelsOn);
+					console.log(channelsNotOn);
+					
+					//create links for active channels
+					var activehtml = '';
+					for(var i = 0; i < channelsOn.length; i++){
+						activehtml += '<div class="channel channelMove">' + channelsOn[i] + '</div>';
+					}
+					$('#activechannellist').html(activehtml);
+					$('.channelMove').click(function(){
+						moveToChannel($(this).html());
+					});
+
+					//create links for non active channels
+					var nonactivehtml = '';
+					for(var i = 0; i < channelsNotOn.length; i++){
+						nonactivehtml += '<div class="channel channelJoin">' + channelsNotOn[i] + '</div>';
+					}
+					$('#channellist').html(nonactivehtml);
+					$('.channelJoin').click(function(){
+						joinToChannel(socket, $(this).html());
+					});
+				});
+
+				//receiving messages, client only receives messages from channels it is on
+				socket.on('message', function(data){
+					console.log(data.username + ': ' + data.message);
+					if(messages[data.channel].length > maxMessagesPerChannel){
+						messages[data.channel].shift();
+					}
+					messages[data.channel].push({type : 'normal', username : data.username, color : data.color, message : data.message});
+					if(data.channel == currentChannel){
+						addNewMessage(data.username, data.color, data.message);
+					}
+				});
+
+				//recieving information messages (joining, leaving, alerts, ...)
+				socket.on('info', function(data){	
+					if(messages[data.channel].length > maxMessagesPerChannel){
+						messages[data.channel].shift();
+					}			
+					messages[data.channel].push({type : 'info', messages : data.messages, colors : data.colors});
+					if(data.channel == currentChannel){
+						addNewInfoMessage(data.messages, data.colors);
+					}
+				});
 				
-				//create links for active channels
-				var activehtml = '';
-				for(var i = 0; i < channelsOn.length; i++){
-					activehtml += '<div class="channel channelMove">' + channelsOn[i] + '</div>';
-				}
-				$('#activechannellist').html(activehtml);
-				$('.channelMove').click(function(){
-					moveToChannel($(this).html());
-				});
-
-				//create links for non active channels
-				var nonactivehtml = '';
-				for(var i = 0; i < channelsNotOn.length; i++){
-					nonactivehtml += '<div class="channel channelJoin">' + channelsNotOn[i] + '</div>';
-				}
-				$('#channellist').html(nonactivehtml);
-				$('.channelJoin').click(function(){
-					joinToChannel(socket, $(this).html());
-				});
-			});
-
-			//receiving messages, client only receives messages from channels it is on
-			socket.on('message', function(data){
-				console.log(data.username + ': ' + data.message);
-				messages[data.channel].push({type : 'normal', username : data.username, color : data.color, message : data.message});
-				if(data.channel == currentChannel){
-					addNewMessage(data.username, data.color, data.message);
-				}
-			});
-
-			//recieving information messages (joining, leaving, alerts, ...)
-			socket.on('info', function(data){	
-				messages[data.channel].push({type : 'info', messages : data.messages, colors : data.colors});
-				if(data.channel == currentChannel){
+				//recieving alerts. Just add a new message to the current channel with their info.
+				socket.on('alert', function(data){
 					addNewInfoMessage(data.messages, data.colors);
-				}
-			});
+				});
 
+				//gui stuff
+				//if user trying to post emit the corresponding 'post'
+				$('#input button').click(function(){
+					var msg = $('#input input').val();
+					$('#input input').val('');
+					socket.emit('post', { channel : currentChannel, message : msg });
+				});
+				$('#input input').keyup(function(e){
+					if(e.keyCode == 13){
+						var msg = $(this).val();
+						$(this).val('');
+						socket.emit('post', { channel : currentChannel, message : msg});
+					}
+				});
 
-			//gui stuff
-			//if user trying to post emit the corresponding 'post'
-			$('#input button').click(function(){
-				var msg = $('#input input').val();
-				$('#input input').val('');
-				socket.emit('post', { channel : currentChannel, message : msg });
-			});
-			$('#input input').keyup(function(e){
-				if(e.keyCode == 13){
-					var msg = $(this).val();
-					$(this).val('');
-					socket.emit('post', { channel : currentChannel, message : msg});
-				}
-			});
+				//user pressed the button to create new channel
+				$('#createnewchannelbutton').click(function(){
+					toggleCreateNew(socket);
+				});
 
-			//user pressed the button to create new channel
-			$('#createnewchannelbutton').click(function(){
-				toggleCreateNew(socket);
-			});
+				//dropdown menu toggle button
+				$('#dropdownmenu svg#menubutton').click(function(){
+					$('#dropdownmenu #dropdowncontent').slideToggle(200);
+				});
 
-			//dropdown menu toggle button
-			$('#dropdownmenu svg#menubutton').click(function(){
-				$('#dropdownmenu #dropdowncontent').slideToggle(200);
+				//user pressed the leave from current channel button
+				$('#leaveCurrentChannelButton').click(function(){
+					$('#dropdownmenu #dropdowncontent').slideToggle(200);			
+					leaveCurrentChannel(socket);
+				});
+				
+				socket.emit('login', {username : usern, color : col});
 			});
-
-			//user pressed the leave from current channel button
-			$('#leaveCurrentChannelButton').click(function(){
-				$('#dropdownmenu #dropdowncontent').slideToggle(200);			
-				leaveCurrentChannel(socket);
-			});
-		});
+		}
 	});	
 }
 
@@ -108,11 +132,20 @@ var toggleCreateNewState = true;
 function toggleCreateNew(socket){
 	if(toggleCreateNewState){
 		toggleCreateNewState = false;
-		$('#createnewchannel').html('<input type="text" autofocus><button>Luo</button>').promise().done(function(){
+		$('#createnewchannel').html('<input type="text"><button>Luo</button>').promise().done(function(){
+			$('#createnewchannel input').focus();
 			$('#createnewchannel button').click(function(){
 				if($('#createnewchannel input').val()){
 					createNewChannel(socket, $('#createnewchannel input').val());
 					toggleCreateNew(socket);
+				}
+			});
+			$('#createnewchannel input').keyup(function(e){
+				if(e.keyCode == 13){
+					if($('#createnewchannel input').val()){
+						createNewChannel(socket, $('#createnewchannel input').val());
+						toggleCreateNew(socket);
+					}
 				}
 			});
 		});
@@ -128,16 +161,22 @@ function toggleCreateNew(socket){
 
 function createNewChannel(socket, chan){
 	var chann = validator.escape(chan + '');
-	console.log('creating channel '+chann);
-	messages[chann] = [];
-	socket.emit('create', {channel : chann});
+	console.log('trying to create channel '+chann);
+	socket.emit('create', {channel : chann}, function(error){
+		if(!error){
+			messages[chann] = [{type : 'info', messages : ['New channel created'], colors : []}];
+		}
+	});
 }
 
 function joinToChannel(socket, chan){
 	console.log('joining channel '+chan);
-	messages[chan] = [];
-	socket.emit('join', {channel : chan});
-	moveToChannel(chan);
+	socket.emit('join', {channel : chan}, function(error){
+		if(!error){
+			messages[chan] = [];
+			moveToChannel(chan);
+		}
+	});
 }
 
 function moveToChannel(channel){
@@ -164,9 +203,12 @@ function leaveCurrentChannel(socket){
 }
 
 function addNewMessage(usern, color, msg){
+	checkForScrollActive();
 	$('#messages').append('<div class="messagewrap"><span class="message"><b style="color:' + color + '">' + usern + ':</b> ' + msg + '</span></div>');
+	updateScroll();
 }
 function addNewInfoMessage(msgs, colors){
+	checkForScrollActive();
 	var mesg = '';
 	for(var i = 0; i < msgs.length; i++){
 		mesg += '<span style="color:';
@@ -178,4 +220,19 @@ function addNewInfoMessage(msgs, colors){
 		mesg += ';">' + msgs[i] + '</span>';
 	}
 	$('#messages').append('<div class="messagewrap"><b><span class="message">'+mesg+'</span></div></b>');
+	updateScroll();
+}
+
+function checkForScrollActive(){
+	var elem = $('#messages');
+	if(elem[0].scrollHeight - elem.scrollTop() == elem.outerHeight()){
+		scrollActive = true;
+	} else {
+		scrollActive = false;
+	}
+}
+function updateScroll(){
+	if(scrollActive){
+		$('#messages').scrollTop($('#messages').prop('scrollHeight'));
+	}
 }
